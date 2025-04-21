ed8245250e5a697c2516763595bbd70659141a94
glebi574 <glebi574@gmail.com>
2025-04-21 16:50:42 +0300

    Added `meadow-customizations`

---
+++ meadow-customizations/PluginInterface.cs
@@ -0,0 +1,106 @@
+﻿using Menu.Remix.MixedUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace meadow_customizations
+{
+  public class PluginInterface : OptionInterface
+  {
+    public enum BodyColorMode
+    {
+      Constant,
+      Random
+    }
+
+    public enum EyeColorMode
+    {
+      Constant,
+      RandomConstant,
+      Random,
+      Wave
+    }
+
+    public readonly Configurable<BodyColorMode> bodyColorMode;
+    public readonly Configurable<EyeColorMode> eyeColorMode;
+
+    public readonly Configurable<bool> useCustomBodyColor;
+    public readonly Configurable<Color> customBodyColor;
+    public readonly Configurable<bool> useCustomEyeColor;
+    public readonly Configurable<Color> customEyeColor;
+    public readonly Configurable<Color> customEyeWaveColor;
+    public readonly Configurable<int> waveSpeed;
+    public readonly Configurable<int> eyeSwitchTimer;
+    public readonly Configurable<bool> useCustomNickname;
+    public readonly Configurable<bool> showArenaStats;
+    public readonly Configurable<bool> useCustomName;
+    public readonly Configurable<bool> showPing;
+    public readonly Configurable<string> customName;
+
+    private UIelement[] options;
+
+    public PluginInterface()
+    {
+      bodyColorMode = config.Bind("bodyColorMode", BodyColorMode.Constant);
+      eyeColorMode = config.Bind("customEyeColorMode", EyeColorMode.Constant);
+      useCustomBodyColor = config.Bind("useCustomBodyColor", false);
+      customBodyColor = config.Bind("customBodyColor", new Color(0f, 0f, 0f));
+      useCustomEyeColor = config.Bind("useCustomEyeColor", false);
+      customEyeColor = config.Bind("customEyeColor", new Color(1f, 1f, 1f));
+      customEyeWaveColor = config.Bind("customEyeWaveColor", new Color(1f, 1f, 1f));
+      waveSpeed = config.Bind("waveSpeed", 69);
+      eyeSwitchTimer = config.Bind("eyeSwitchTimer", 10);
+      useCustomNickname = config.Bind("useCustomNickname", false);
+      showArenaStats = config.Bind("showArenaStats", false);
+      useCustomName = config.Bind("useCustomName", false);
+      showPing = config.Bind("showPing", false);
+      customName = config.Bind("customName", "");
+    }
+
+    public override void Initialize()
+    {
+      OpTab optionsTab = new OpTab(this, "Options");
+      Tabs = new OpTab[] { optionsTab };
+
+      options = new UIelement[] {
+        new OpLabel(10f, 550f, "Options", bigText: true),
+
+        new OpCheckBox(useCustomBodyColor, new Vector2(10f, 510f)),
+        new OpLabel(40f, 510f, "Custom body color", bigText: true),
+        new OpLabel(40f, 500f, "Note: only changes in the start of round"),
+        new OpColorPicker(customBodyColor, new Vector2(10f, 350f)),
+        new OpLabel(170f, 480f, "Body color mode"),
+        new OpComboBox(bodyColorMode, new Vector2(170f, 450f), 130f, OpResourceSelector.GetEnumNames(null, typeof(BodyColorMode)).ToList()),
+
+        new OpCheckBox(useCustomEyeColor, new Vector2(310f, 510f)),
+        new OpLabel(340f, 510f, "Custom eye color", bigText: true),
+        new OpLabel(340f, 480f, "Base eye color"),
+        new OpColorPicker(customEyeColor, new Vector2(310f, 330f)),
+        new OpLabel(340f, 300f, "Eye wave color"),
+        new OpColorPicker(customEyeWaveColor, new Vector2(310f, 150f)),
+        new OpLabel(470f, 480f, "Wave speed"),
+        new OpSlider(waveSpeed, new Vector2(470f, 440f), 130) {min = 10, max = 400},
+        new OpLabel(470f, 410f, "Color switch timer"),
+        new OpSlider(eyeSwitchTimer, new Vector2(470f, 370f), 130) {min = 0, max = 100},
+        new OpLabel(470f, 180f, "Eye color mode"),
+        new OpComboBox(eyeColorMode, new Vector2(470f, 150f), 130f, OpResourceSelector.GetEnumNames(null, typeof(EyeColorMode)).ToList()),
+
+        new OpCheckBox(useCustomNickname, new Vector2(10f, 310f)),
+        new OpLabel(40f, 310f, "Custom nickname", bigText: true),
+        new OpLabel(40f, 300f, "Note: only changes name, shown over you"),
+        new OpCheckBox(showArenaStats, new Vector2(10f, 270f)),
+        new OpLabel(40f, 272f, "Show kills/deaths in Arena"),
+        // new OpCheckBox(showPing, new Vector2(10f, 240f)),
+        // new OpLabel(40f, 242f, "Show ping"),
+        new OpCheckBox(useCustomName, new Vector2(10f, 210f)),
+        new OpLabel(40f, 212f, "Use custom name"),
+        new OpTextBox(customName, new Vector2(10f, 180f), 150f)
+      };
+      
+      optionsTab.AddItems(options);
+    }
+  }
+}
