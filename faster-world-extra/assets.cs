4c7bdc6b3a50d9176e517d2519e13202ec80f3b3
glebi574 <glebi574@gmail.com>
2026-05-21 22:23:54 +0300

    Added `faster-world-extra`

---
+++ faster-world-extra/assets.cs
@@ -0,0 +1,117 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace faster_world_extra;
+
+public static class M_Assets
+{
+  public static readonly string gameVersion = (string)typeof(RainWorld).GetField(nameof(RainWorld.GAME_VERSION_STRING)).GetValue(null);
+  public static readonly Dictionary<string, string> cachedPaths = [];
+  public static ModManager.Mod[] lastModList = [];
+  public static string mergedmodsPath, consolefilesPath;
+
+  static M_Assets()
+  {
+    mergedmodsPath = Path.Combine(UnityEngine.Application.streamingAssetsPath, "mergedmods\\");
+  }
+
+  public static void ModManager_RefreshModsLists(ILContext il)
+  {
+    ILCursor c = new(il);
+
+    if (c.TryGotoNext(i => i.OpCode == OpCodes.Ret))
+      c.Emit(OpCodes.Call, ((Delegate)IndexMods).Method);
+  }
+
+  public static void IndexMods()
+  {
+    if (lastModList.Length != ModManager.ActiveMods.Count)
+      goto _index;
+    for (int i = 0; i < lastModList.Length; ++i)
+      if (lastModList[i].id != ModManager.ActiveMods[i].id)
+        goto _index;
+    return;
+  _index:
+    cachedPaths.Clear();
+    int versionLength = gameVersion.Length + 1;
+    string modsPath = null, workshopPath = null;
+    Dictionary<string, string>[] paths = new Dictionary<string, string>[ModManager.ActiveMods.Count],
+      versionedPaths = new Dictionary<string, string>[paths.Length],
+      newestPaths = new Dictionary<string, string>[paths.Length];
+    Parallel.For(0, ModManager.ActiveMods.Count, i =>
+    {
+      int index = ModManager.ActiveMods.Count - i - 1;
+      Dictionary<string, string> modPaths = paths[index] = [], modVersionedPaths = versionedPaths[index] = [], modNewestPaths = newestPaths[index] = [];
+      string path = ModManager.ActiveMods[index].path + '\\';
+      DirectoryInfo parent = Directory.GetParent(path);
+      if (parent.Parent.Name == "mods")
+        modsPath = parent.Parent.FullName;
+      else
+        workshopPath = parent.Parent.FullName;
+
+      foreach (string filePath in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
+      {
+        string localPath = filePath.Substring(path.Length).ToLowerInvariant();
+        modPaths[localPath] = localPath;
+        if (localPath.StartsWith(gameVersion))
+          modVersionedPaths[localPath.Substring(versionLength)] = localPath;
+        else if (localPath.StartsWith("newest"))
+          modNewestPaths[localPath.Substring(7)] = localPath;
+      }
+      foreach (KeyValuePair<string, string> local in modNewestPaths)
+        modPaths[local.Key] = local.Value;
+      foreach (KeyValuePair<string, string> local in modVersionedPaths)
+        modPaths[local.Key] = local.Value;
+      foreach (KeyValuePair<string, string> local in (KeyValuePair<string, string>[])[.. modPaths])
+        modPaths[local.Key] = path + local.Value;
+    });
+    foreach (Dictionary<string, string> modPaths in paths)
+      foreach (KeyValuePair<string, string> cachedPath in modPaths)
+        cachedPaths[cachedPath.Key] = cachedPath.Value;
+    lastModList = [.. ModManager.ActiveMods];
+    consolefilesPath = AssetManager.GetConsoleFilesSubfolder() is string consoleFilesSubfolder
+      ? Path.Combine(Custom.rootFolderDirectory, "consolefiles", consoleFilesSubfolder + '\\')
+      : null;
+  }
+
+  public static string WorldLodaer_FindRoomFile(string roomName, bool includeRootDirectory, string additionalAppend, bool showWarning = true)
+  {
+    string region = (roomName = roomName.ToLowerInvariant()).SubstringUntil('_'), roomFile = roomName + additionalAppend, text;
+    if ( (text = ResolveFilePath(string.Concat("world\\", region, "-rooms\\", roomFile))) != null
+      || region == "gate"
+      && (text = ResolveFilePath("world\\gates\\" + roomFile)) != null
+      || (text = ResolveFilePath("world\\gates\\gate_shelters\\" + roomFile)) != null
+      || (text = ResolveFilePath("levels\\" + roomFile)) != null
+      || ModManager.MSC && roomName.Contains("challenge")
+      && (text = ResolveFilePath("levels\\challenges\\" + roomFile)) != null)
+      return includeRootDirectory ? "file:///" + text : text;
+    return null;
+  }
+
+  public static string ResolveFilePath(string path)
+  {
+    string text;
+    if ( File.Exists(text = mergedmodsPath + path)
+      || cachedPaths.TryGetValue(path.ToLowerInvariant().Replace('/', '\\'), out text)
+      || consolefilesPath != null && File.Exists(text = consolefilesPath + path)
+      || File.Exists(text = Path.Combine(Custom.rootFolderDirectory, path)))
+      return text;
+    return null;
+  }
+
+  public static string AssetManager_ResolveFilePath(string path, bool skipMergedMods = false, bool skipConsoleFiles = false)
+  {
+    string text;
+    path = path.ToLowerInvariant();
+    if ( !skipMergedMods && File.Exists(text = mergedmodsPath + path)
+      || cachedPaths.TryGetValue(path.Replace('/', '\\'), out text)
+      || !skipConsoleFiles && consolefilesPath != null && File.Exists(text = consolefilesPath + path))
+      return text;
+    return Path.Combine(Custom.rootFolderDirectory, path);
+  }
+}
---
+++ faster-world-extra/main.cs
@@ -0,0 +1,120 @@
+﻿//#define _DEV
+
+global using static faster_world_extra.CommonWrapper;
+using BepInEx;
+using gelbi_silly_lib;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Reflection;
+
+#if _DEV
+using profiler;
+#endif
+
+namespace faster_world_extra
+{
+  public static class CommonWrapper
+  {
+    public static BepInEx.Logging.ManualLogSource Log;
+
+    public static string SubstringUntil(this string self, char c)
+    {
+      int i = self.IndexOf(c);
+      if (i == -1)
+        return self;
+      return self.Substring(0, i);
+    }
+  }
+
+  [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+    public const string PLUGIN_GUID = "gelbi.faster-world-extra", PLUGIN_NAME = "Faster World Extra", PLUGIN_VERSION = "1.0.0", targetVersion = "1.11.8";
+    public static bool isInit = false, gslEnabled = false;
+    public static BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+
+    public void OnEnable()
+    {
+      Log = Logger;
+
+      try
+      {
+        On.Futile.ctor += Futile_ctor;
+      }
+      catch (Exception e)
+      {
+        Logger.LogError(e);
+      }
+    }
+    public static Action<Delegate, Delegate> ReplaceD;
+    public static Action<Type, Type[], Delegate> ReplaceC;
+    public static Action<Type, string, Delegate> ReplaceF;
+    public static Action<Type, string, Type[], Delegate> ReplaceFL;
+
+    public static void VersionSpecificInit()
+    {
+      VersionSpecific.Update(targetVersion, VersionSpecific.MismatchAction.Warn);
+      ReplaceD = (from, to) => VersionSpecific.newNativeDetour(from, to);
+      ReplaceC = (type, args, target) => VersionSpecific.newNativeDetour(type.GetConstructor(flags, null, args, null), target.Method);
+      ReplaceF = (type, methodName, target) => VersionSpecific.newNativeDetour(type.GetMethod(methodName, flags), target.Method);
+      ReplaceFL = (type, methodName, parameterTypes, target) => VersionSpecific.newNativeDetour(type.GetMethod(methodName, flags, null, parameterTypes, null), target.Method);
+    }
+
+    public static void SetVersionFlagImportant()
+    {
+      VersionSpecific.Update(targetVersion, VersionSpecific.MismatchAction.Skip);
+    }
+
+    public static void Futile_ctor(On.Futile.orig_ctor orig, Futile self)
+    {
+      orig(self);
+
+      if (isInit)
+        return;
+      isInit = true;
+
+      try
+      {
+        foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+          if (asm.GetName().Name == "gelbi-silly-lib-preloader")
+          {
+            gslEnabled = true;
+            break;
+          }
+        if (gslEnabled)
+          VersionSpecificInit();
+        else
+        {
+          ReplaceD = (from, to) => new NativeDetour(from, to);
+          ReplaceC = (type, args, target) => new NativeDetour(type.GetConstructor(flags, null, args, null), target.Method);
+          ReplaceF = (type, methodName, target) => new NativeDetour(type.GetMethod(methodName, flags), target.Method);
+          ReplaceFL = (type, methodName, parameterTypes, target) => new NativeDetour(type.GetMethod(methodName, flags, null, parameterTypes, null), target.Method);
+        }
+
+        #region 1.0.0
+
+        ReplaceF(typeof(WorldLoader), "FindRoomFile", M_Assets.WorldLodaer_FindRoomFile);
+        ReplaceFL(typeof(AssetManager), "ResolveFilePath", [typeof(string), typeof(bool), typeof(bool)], M_Assets.AssetManager_ResolveFilePath);
+
+        IL.ModManager.RefreshModsLists += M_Assets.ModManager_RefreshModsLists;
+
+        #endregion
+
+#if _DEV
+        //MethodPatcher.PatchMethods([
+        //  typeof(WorldLoader).GetMethod("FindRoomFile", flags),
+        //  typeof(AssetManager).GetMethod("ResolveFilePath", flags, null, [typeof(string), typeof(bool), typeof(bool)], null),
+        //]);
+
+        MethodPatcher.PatchMethods([
+          M_Assets.AssetManager_ResolveFilePath,
+        ]);
+#endif
+      }
+      catch (Exception e)
+      {
+        Log.LogError(e);
+      }
+    }
+  }
+}
